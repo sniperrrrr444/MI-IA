ed6014de943a48fd7b0e4fa4d1a8ff51e1381b45
sniperrrrr444 <diego.barcenaf@sanfelix.org>
2026-08-27 13:43:33 +0200

    feat: add offline app shell

---
+++ frontend/sw.js
@@ -0,0 +1,1 @@
+const CACHE="miia-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
